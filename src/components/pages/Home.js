ca10d2ec78bc33ded7078374281c9016e9864b8e
EZEH OBINNA CHARLES <63585250+charles-purity@users.noreply.github.com>
2023-07-08 12:41:45 +0100

    updating files in Git

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import LandingSection from '../LandingSection';
+import Cards from '../Cards';
 
 
 function Home() {
   return (
     <>
       <LandingSection />
-     
+      <Cards />
     </>
   );
 }

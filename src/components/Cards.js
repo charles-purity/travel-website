79983133bd949023463ad3ab257cad5fb3eee06c
EZEH OBINNA CHARLES <63585250+charles-purity@users.noreply.github.com>
2023-07-09 14:26:37 +0100

    Uploading files

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards() {
   return (
@@ -8,7 +9,44 @@ function Cards() {
         <div className='cards__container'>
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
-                    <CardItem />
+                    <CardItem 
+                      src='images/img-9.jpg'
+                      text='Explore the hideen waterfall deep inside the Amazon jungle'
+                      label='Adventure'
+                      path='/services'
+                    />
+                     <CardItem 
+                      src='images/img-2.jpg'
+                      text='Travel through the Islands of Bali in a private cruise'
+                      label='Luxury'
+                      path='/services'
+                    />
+                    <CardItem 
+                      src='images/img-3.jpg'
+                      text='Set Sail in the Atlantic Ocean visiting'
+                      label='Luxury'
+                      path='/services'
+                    />
+                </ul>
+                <ul className='cards__items'>
+                    <CardItem 
+                      src='images/img-1.jpg'
+                      text='Explore the hideen waterfall deep inside the Amazon jungle'
+                      label='Adventure'
+                      path='/services'
+                    />
+                     <CardItem 
+                      src='images/img-6.jpg'
+                      text='Experience nature and meeting new people'
+                      label='Tourism'
+                      path='/services'
+                    />
+                    <CardItem 
+                      src='images/img-4.jpg'
+                      text='Explore the hidden arena of sport'
+                      label='Sport'
+                      path='/services'
+                    />
                 </ul>
             </div>
         </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 import LandingSection from '../LandingSection';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
     <>
       <LandingSection />
       <Cards />
+      <Footer />
     </>
   );
 }
